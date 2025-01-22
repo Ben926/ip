ba497656bd3661efb2475f03f02fae4a5bf893f3
Ben926 <benjamin.koh.ts@gmail.com>
2025-01-22 19:51:23 +0800

    Echo

--- src/main/java/Ronaldo.java
+++ src/main/java/Ronaldo.java
@@ -1,7 +1,15 @@
+import java.util.*;
+
 public class Ronaldo {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Ronaldo");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("SIIUUUUUU!!! Ronaldo here.");
+        System.out.println("How can I help you?\n");
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input + "\n");
+            input = sc.nextLine();
+        }
+        System.out.println("Goodbye SIUUUU.");
     }
 }

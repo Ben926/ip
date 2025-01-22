d619678a97dee117d7d05860cb3bf5b2155d41b9
Ben926 <benjamin.koh.ts@gmail.com>
2025-01-22 19:38:02 +0800

    rename, greet, exit

---
+++ src/main/java/Ronaldo.java
@@ -0,0 +1,7 @@
+public class Ronaldo {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Ronaldo");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}

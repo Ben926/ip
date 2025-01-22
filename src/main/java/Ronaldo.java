38c70c3ce0d61dae76ea7007996e30ec6681bfb4
Ben926 <benjamin.koh.ts@gmail.com>
2025-01-22 22:46:17 +0800

    mark as done

--- src/main/java/Ronaldo.java
+++ src/main/java/Ronaldo.java
@@ -14,6 +14,16 @@ public class Ronaldo {
                     System.out.println(String.format("%d. %s", i + 1, arr.get(i)));
                 }
                 System.out.println("");
+            } else if (tokens[0].equals("mark")) {
+                Task task = arr.get(Integer.parseInt(tokens[1]) - 1);
+                task.markAsDone();
+                System.out.println(String.format("Ok, marked this " +
+                        "task as done:\n%s\n", task));
+            } else if (tokens[0].equals("unmark")) {
+                Task task = arr.get(Integer.parseInt(tokens[1]) - 1);
+                task.markAsUndone();
+                System.out.println(String.format("Ok, marked this " +
+                        "task as undone:\n%s\n", task));
             } else {
                 arr.add(new Task(input));
                 System.out.println("Added: " + input + "\n");
@@ -26,14 +36,28 @@ public class Ronaldo {
 
     public static class Task {
         private String description;
+        private boolean isDone;
 
         public Task(String description) {
             this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        public void markAsUndone() {
+            this.isDone = false;
         }
 
         @Override
         public String toString() {
-            return this.description;
+            return String.format("[%s] %s", getStatusIcon(), this.description);
         }
     }
 }
